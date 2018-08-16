e46ad74e072f9686e47b8b0f8adbbd26b53f612d
LC1301328 <LC1301328>
2018-08-16 10:17:38 -0400

    hola

---
+++ ejemplo_hola_mundo/src/NewMain.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Duoc
+ */
+public class NewMain {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+       System.out.println("Hola Mundo");
+       System.out.println("Ejemplo para GitHub / GitLub");
+    }
+    
+}
